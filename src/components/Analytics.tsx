4b7a463a26fffb9c3b434c138addc0857e6ee5bc
Bryan Magistrado <bmagistrado@yahoo.com>
2023-06-09 01:19:12 +0100

    Worked on the Analytics Component
    -Did some layout work on the Analytics component and changed the colour scheme around to fit the new 3D image.

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import styles from '../styles';
 import computer from '../assets/analytics.png';
 
+import styles from '../styles';
 
 const Analytics = () => {
   return (
     <div className={`${styles.sectionStyle} bg-white mt-[2rem] md:mt-[8rem]`}>
         <div className={`flex flex-col md:flex-row justify-center items-center`}>
             <div className='text-black'>
-                Analytics
+                <p>DATA ANALYTICS DASHBOARD</p>
+                <h1>Manage Data Analytics Centrally</h1>
+                <p>In today's fast-paced world, data is the key to success. Our cutting-edge analytics platform combines state-of-the-art technology with industry expertise, 
+                  enabling you to extract meaningful insights from your data effortlessly. Uncover hidden patterns, identify trends, and make data-driven decisions that propel your business forward.</p>
+                <button className={`${styles.analyticsButton}`}>Get Started</button>
+            </div>
+            <div>
+              <img src={computer} alt="Analytics Image" />
             </div>
         </div>
     </div>
--- src/styles.js
+++ src/styles.js
@@ -3,27 +3,27 @@ const styles = {
     "flex items-center justify-center mx-auto max-w-[1240px] h-full px-4 text-white",
 
     /**
-     * * NAVBAR STYLES
+     * * NAVBAR STYLES 84d1c9 c9befa
      */
 
     mobileNav:
     "fixed overflow-x-hidden left-0 right-0 top-0 w-[14rem] h-full border-r border-r-gray-900 bg-[#000300] transform transition-all duration-500 z-30",
 
     navButton:
-    "w-[9.5rem] py-[0.75rem] px-[1.25rem] cursor-pointer bg-white text-black rounded-lg ml-2 font-medium text-[0.975rem] text-center hover:text-white hover:bg-gradient-to-r from-[#774caf] to-[#68628d] transition duration-500",
+    "w-[9.5rem] py-[0.75rem] px-[1.25rem] cursor-pointer bg-white text-black rounded-lg ml-2 font-medium text-[0.975rem] text-center hover:text-white hover:bg-gradient-to-r from-[#774caf] to-[#84d1c9] transition duration-500",
 
     navLink:
     "p-3 text-center cursor-pointer w-[7rem] hover:text-[#c9befa] transition-all duration-300 inline-block",
 
     navSpan:
-    "w-min-content relative transition-all before:absolute before:top-[1.5rem] before:right-0 before:h-[0.1rem] before:w-0 before:bg-gradient-to-r before:from-purple-800 before:via-indigo-500 before:to-blue-500 before:transition-all before:duration-500 hover:before:left-0 hover:before:w-full",
+    "w-min-content relative transition-all before:absolute before:top-[1.5rem] before:right-0 before:h-[0.1rem] before:w-0 before:bg-gradient-to-r before:from-purple-800 before:via-indigo-500 before:to-[#84d1c9] before:transition-all before:duration-500 hover:before:left-0 hover:before:w-full",
 
     /**
      * * HERO STYLES
      */
 
     heroButton:
-    "w-[10.5rem] py-[1rem] px-[1.75rem] cursor-pointer bg-white text-black tracking-widest rounded-lg font-medium text-[0.975rem] hover:text-white hover:bg-gradient-to-r from-[#774caf] to-[#68628d] transition duration-500",
+    "w-[10.5rem] py-[1rem] px-[1.75rem] cursor-pointer bg-white text-black tracking-widest rounded-lg font-medium text-[0.975rem] hover:text-white hover:bg-gradient-to-r from-[#774caf] to-[#84d1c9] transition duration-500",
 
     heroContainer:
     "max-w-[800px] w-[100%] h-full text-center flex flex-col justify-center",
@@ -38,7 +38,14 @@ const styles = {
     "md:text-2xl sm:text-xl text-sm font-bold py-4",
 
     heroParagraph:
-    "md:text-lg text-xs font-bond text-gray-400 w-[75%] lg:w-full place-self-center"
+    "md:text-lg text-xs font-bond text-gray-400 w-[75%] lg:w-full place-self-center",
+
+    /**
+     * ANALYTICS STYLES
+     */
+
+    analyticsButton:
+    "w-[10.5rem] py-[1rem] px-[1.75rem] cursor-pointer bg-black text-white tracking-widest rounded-lg font-medium text-[0.975rem] hover:text-black hover:bg-gradient-to-r from-[#774caf] to-[#84d1c9] transition duration-500",
 
 
 };
