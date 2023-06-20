1c1f9b32e32ee12b4ec6a97c90b4e64c9f98958d
Bryan Magistrado <u1854321@unimail.hud.ac.uk>
2023-06-20 15:28:16 +0100

    Changed the 'Most Popular' element
    -Made some styling changes to the pricing component, still more to be added such as background and title styling.
    -Need to add 'Most Popular' styling for smaller screen sizes too.

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import { FaStar, FaUser, FaUsers, FaUserFriends } from "react-icons/fa";
-
+import { TiTick } from "react-icons/ti";
+import backgroundGradient from '../assets/blurry-gradient-haikei.svg';
 import styles from '../styles';
 
 const Pricing: React.FC = () => {
@@ -34,7 +35,7 @@ const Pricing: React.FC = () => {
         },
         {
             id: 'Group Account',
-            image: <FaUsers size={80}/>,
+            image: <FaUsers size={80} />,
             price: '£299',
             storage: '5 TB Storage',
             users: '10 Users Allowed',
@@ -43,24 +44,38 @@ const Pricing: React.FC = () => {
     ]
 
     return (
-        <div className='flex items-center justify-center mx-auto max-w-[100vw] h-full px-4 text-black bg-white'>
+        <div className='flex flex-col items-center justify-center mx-auto max-w-[100vw] h-full px-4 text-black bg-white'>
+            <h1 className='md:text-5xl text-4xl font-bold my-6 md:my-10 text-black'>Choose your plan...</h1>
             <div className='flex lg:flex-wrap lg:flex-row flex-col gap-3 my-6 md:my-10 w-full justify-center'>
                 {cardInfo.map((item, index) => (
-                    <div key={item.id} className={`flex flex-col justify-between items-center gap-6 w-full lg:w-[20rem] min-h-full p-5
+                    <div key={item.id} className={`flex flex-col justify-between items-center gap-6 w-full lg:w-[20rem] min-h-full p-5 my-3 lg:my-0
                      ${index === 1 ? 'text-white bg-black' : 'text-black'} rounded-lg relative`}>
-                        <div className={`${index === 1 ? 'absolute top-[4vw] right-[4vw] sm:top-[1vh] sm:right-[1vw] lg:top-[1vh] lg:right-[1vw] xl:right-[0.5vw] 2xl:top-[0.75vh] 2xl:right-[0.5vw] bg-white text-black p-3 lg:p-2 rounded-md flex flex-row items-center justify-between gap-3 lg:gap-2' : 'hidden'}`}>
+                        {/* <div className={`${index === 1 ? 'absolute top-[4vw] right-[4vw] sm:top-[1vh] sm:right-[1vw] lg:top-[1vh] lg:right-[1vw] xl:right-[0.5vw] 2xl:top-[0.75vh] 2xl:right-[0.5vw] bg-white text-black p-3 lg:p-2 rounded-md flex flex-row items-center justify-between gap-3 lg:gap-2' : 'hidden'}`}>
                             <FaStar color="#774caf" />
                             <p className='hidden sm:block lg:hidden'>Most Popular</p>
                             <p className='tracking-wider hidden lg:block'>Popular</p>
+                        </div> */}
+                        <div className={`${index === 1 ? 'flex flex-row justify-center items-center gap-2 min-w-full max-h-[100px] p-2 rounded-tr-md rounded-tl-md purple-gradient absolute top-[-5vh]' : 'hidden'}`}>
+                            <FaStar color="white" size={20}/>
+                            <p className='text-white font-base text-base md:text-xl'>Most Popular</p>
                         </div>
-                        
+
                         {item.image}
-                        <p className='md:text-3xl text-sm font-bold purple-text-gradient'>{item.id}</p>
+                        <p className='text-3xl font-bold purple-text-gradient'>{item.id}</p>
                         <h1 className='text-3xl font-semibold'>{item.price}</h1>
                         <div className='text-center'>
-                            <p>{item.storage}</p>
-                            <p>{item.users}</p>
-                            <p>{item.send}</p>
+                            <div className='flex flex-row justify-start items-center gap-x-2'>
+                                <TiTick color='#774caf' />
+                                <p>{item.storage}</p>
+                            </div>
+                            <div className='flex flex-row justify-start items-center gap-x-2'>
+                                <TiTick color='#774caf' />
+                                <p>{item.users}</p>
+                            </div>
+                            <div className='flex flex-row justify-start items-center gap-x-2'>
+                                <TiTick color='#774caf' />
+                                <p>{item.send}</p>
+                            </div>
                         </div>
                         <button className={`${index === 1 ? styles.primaryButton : 'hidden'}`}>Start Trial</button>
                         <button className={`${index !== 1 ? styles.secondaryButton : 'hidden'}`}>Start Trial</button>
